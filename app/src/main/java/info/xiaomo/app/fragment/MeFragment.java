7935e20f22b7715013ee50ee8225df3a0c289b66
xiaomo <83387856@qq.com>
2017-01-07 18:07:46 +0800

    menu

---
+++ app/src/main/java/info/xiaomo/app/fragment/MeFragment.java
@@ -0,0 +1,22 @@
+package info.xiaomo.app.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author 小莫 (https://xiaomo.info) (https://github.com/syoubaku)
+ * @version : 2017/1/7 17:56
+ */
+
+public class MeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
