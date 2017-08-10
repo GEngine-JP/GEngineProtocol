ffbe06436ca54bd40a18534b5212f75a19b2f86d
xiaomo <xiaomo@xiaomo.info>
2017-08-10 20:10:18 +0800

    protobuf3 生成工具 :fire:

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# MessageXml
-消息协议配置文件
+# 消息协议配置文件
 
+#### 目录结构预览
+
+├─Code           生成的代码
+├─generate.bat   生成代码的执行文件
+│  ├─cs          c#代码
+│  └─java        java代码
+├─proto          proto文件
+└─Tools
+    └─ProtoGen c#生成工具
+    └─ protoc.exe java生成工具
+
+#### 使用方式
+运行 generate.bat即可生成java和c#的通讯文件
 
 #### 模块
 - 1=user
--- generate.bat
+++ generate.bat
@@ -1,25 +1,39 @@
 @echo off
 
-::Э���ļ�·��, ���Ҫ����\������
-set SOURCE_FOLDER=.\message
+::协议文件路径, 最后不要跟“\”符号
+set SOURCE_FOLDER=proto
+::Java编译器路径
+set JAVA_COMPILER_PATH=Tools\protoc.exe
+::C#编译器路径
+set CS_COMPILER_PATH=Tools\ProtoGen\protogen.exe
 
-::C#������·��
-::set CS_COMPILER_PATH=..\Tools\protobuf-net\ProtoGen\protogen.exe
-::C#�ļ�����·��, ���Ҫ����\������
-::set CS_TARGET_PATH=..\Client\Assets\Scripts\Net\Protocol
+::如果目录不存在则创建
+if not exist Code md Code
+if not exist Code\cs md Code\cs
+if not exist Code\java md Code\java
 
-::Java������·��
-set JAVA_COMPILER_PATH=.\proto\protoc.exe
-::Java�ļ�����·��, ���Ҫ����\������
-set JAVA_TARGET_PATH=F:\ChessGame\ConfigProtocol\message
+::C#文件生成路径, 最后不要跟“\”符号
+set CS_TARGET_PATH=Code\cs
+::Java文件生成路径, 最后不要跟“\”符号
+set JAVA_TARGET_PATH=Code\java
 
-::ɾ��֮ǰ�������ļ�
+::删除之前创建的文件
+del %CS_TARGET_PATH%\*.* /f /s /q
 del %JAVA_TARGET_PATH%\*.* /f /s /q
 
-for /f "delims=" %%i in ('dir /b %SOURCE_FOLDER%\*.proto') do (
-    %JAVA_COMPILER_PATH% --java_out=%JAVA_TARGET_PATH% %%i
+::遍历所有文件
+for /f "delims=" %%i in ('dir /b "%SOURCE_FOLDER%\*.proto"') do (
+    
+    ::生成 C# 代码
+    echo %CS_COMPILER_PATH% -i:%SOURCE_FOLDER%/%%i -o:%CS_TARGET_PATH%\%%~ni.cs
+    %CS_COMPILER_PATH% -i:%SOURCE_FOLDER%\%%i -o:%CS_TARGET_PATH%\%%~ni.cs
+
+    ::生成 Java 代码
+    echo %JAVA_COMPILER_PATH% --java_out=%JAVA_TARGET_PATH% %%i
+    %JAVA_COMPILER_PATH% --java_out=%JAVA_TARGET_PATH%  %SOURCE_FOLDER%\%%i
+    
 )
 
-echo  Э��������ϡ�������
+echo Finish
 
 pause
